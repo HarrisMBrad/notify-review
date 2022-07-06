06a710069750d25c95035a372751577fbfde296f
Brad Harris <bmichaelh13@gmail.com>
2022-07-06 15:34:10 -0400

    removed brackes from screens onjects, only returning app state for now

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { AppState } from './src/AppState/AppState';
+import AppState from './src/AppState/AppState';
 import registerNNPushToken from 'native-notify';
-import { Note } from './src/screens/Note/Note';
+import Note from './src/screens/Note/Note';
 
 
 export default function App() {
@@ -10,27 +10,8 @@ export default function App() {
   
 
   return (
-    <View style={styles.container}>
-      <AppState>
-        <StatusBar style="auto" />
-        <Text>
-          Open up and change me...
-        </Text>
-        <Note>
-          <Text>
-            Hello Homie.
-          </Text>
-        </Note>
-      </AppState>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <AppState />
+  
+  )
+};
